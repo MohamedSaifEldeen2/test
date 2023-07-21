10224c4916ec78802c976672d6fe031a149d0e65
Mohamed Saif-Eldeen <115887254+MohamedSaifEldeen2@users.noreply.github.com>
2023-07-21 18:02:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function displayGrades(){
 const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMDIwODA0MSIsImF1dGgiOiJST0xFX1NUVURFTlQiLCJleHAiOjE2OTE0MzkxMzZ9.bopdS73VdHwCI-IEupiKIGzpS30SoDDI9l2dKKEkpDAx4mHmZ2oIIcewFliga6Z8WwgLbrjQFZT8reADUTMpOQ';
-const apiUrl = 'https://193.227.14.58/api/student-courses?size=150&studentId.equals=20208041&includeWithdraw.equals=true';
+const apiUrl = 'http://193.227.14.58/api/student-courses?size=150&studentId.equals=20208041&includeWithdraw.equals=true';
 
 fetch(apiUrl, {
   headers: {
