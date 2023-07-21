120f54356094134a96672bd4a5e4b77e05f9a575
Mohamed Saif-Eldeen <115887254+MohamedSaifEldeen2@users.noreply.github.com>
2023-07-21 16:40:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function displayGrades(){
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMDIwODA0MSIsImF1dGgiOiJST0xFX1NUVURFTlQiLCJleHAiOjE2OTE0MzkxMzZ9.bopdS73VdHwCI-IEupiKIGzpS30SoDDI9l2dKKEkpDAx4mHmZ2oIIcewFliga6Z8WwgLbrjQFZT8reADUTMpOQ';
+const apiUrl = 'http://193.227.14.58/api/student-courses?size=150&studentId.equals=20208041&includeWithdraw.equals=true';
+
+fetch(apiUrl, {
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    // Process the data as needed
+    console.log(data);
+    // Display the data in HTML
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = JSON.stringify(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
